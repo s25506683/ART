5dce1622ac4701e02501375516f567a51452e75c
Carson <s25506683@gmail.com>
2019-06-07 11:05:59 +0800

    第一次commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+
+    }
+    public void register(View v){
+        Intent it = new Intent(this,Register.class);
+        startActivity(it);
+    }
+    public void fillOrder(View v){
+        Intent it = new Intent(this,FillOrder.class);
+        startActivity(it);
+    }
+
+
+}
