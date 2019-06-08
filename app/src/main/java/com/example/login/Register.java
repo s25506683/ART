de8938f703c4a15edc553460ffa2c2001d826041
jay18309 <cookopen2>
2019-06-09 01:08:34 +0800

    第七次commit (上傳圖片)

--- app/src/main/java/com/example/login/Register.java
+++ app/src/main/java/com/example/login/Register.java
@@ -1,13 +1,50 @@
 package com.example.login;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Register extends AppCompatActivity {
+    
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 }
--- app/src/main/java/com/example/login/fillProject.java
+++ app/src/main/java/com/example/login/fillProject.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class fillProject extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class fillProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_project);
+
     }
+
+
+
 }
+
