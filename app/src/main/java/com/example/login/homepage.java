d6907976efeb11f90974939aa7999b695d84aa7c
jay18309 <cookopen2>
2019-06-08 18:51:41 +0800

    第六次commit

--- app/src/main/java/com/example/login/homepage.java
+++ app/src/main/java/com/example/login/homepage.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
+    public void information(View v){
+        Intent it = new Intent(this,Information.class);
+        startActivity(it);
+    }
+
 }
+
