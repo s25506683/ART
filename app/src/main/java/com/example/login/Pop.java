49c78505e9cc082218293ef18ac396bd4daaf68d
jay18309 <cookopen2>
2019-06-09 15:40:37 +0800

    第八次commit (彈出視窗)

---
+++ app/src/main/java/com/example/login/Pop.java
@@ -0,0 +1,24 @@
+package com.example.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class Pop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwindow);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.widthPixels;
+
+        getWindow().setLayout((int) (width*.6),(int)(height*.4));
+
+    }
+}
--- app/src/main/java/com/example/login/fillProject.java
+++ app/src/main/java/com/example/login/fillProject.java
@@ -1,8 +1,10 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class fillProject extends AppCompatActivity {
@@ -11,6 +13,15 @@ public class fillProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_project);
+
+        Button b =(Button)findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(fillProject.this,Pop.class));
+            }
+        });
 
     }
 
