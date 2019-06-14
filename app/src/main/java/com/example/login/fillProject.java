4eb3e32658bbd24a9c982acb5f1c1fea9b75c2a3
Carson <s25506683@gmail.com>
2019-06-14 13:01:15 +0800

    第15次commit

--- app/src/main/java/com/example/login/fillProject.java
+++ app/src/main/java/com/example/login/fillProject.java
@@ -2,13 +2,14 @@ package com.example.login;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class fillProject extends AppCompatActivity {
 
@@ -21,6 +22,13 @@ public class fillProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_project);
+        Spinner notify = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
+                this, R.array.unit, android.R.layout.simple_spinner_item );
+        nAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        notify.setAdapter(nAdapter);
+
 
         Button b =(Button)findViewById(R.id.button);
 
@@ -33,36 +41,31 @@ public class fillProject extends AppCompatActivity {
 
 
 
-            imageView = (ImageView)findViewById(R.id.imageView);
-            button2 = (Button)findViewById(R.id.button2);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        button2 = (Button)findViewById(R.id.button2);
 
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openGallery();
-                }
-            });
-
-        }
-
-        private void openGallery(){
-            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            startActivityForResult(gallery, PICK_IMAGE);
-
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode,Intent data){
-            super.onActivityResult(requestCode, resultCode, data);
-            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-                imageUri = data.getData();
-                imageView.setImageURI(imageUri);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
             }
-        }
+        });
 
     }
 
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
 
-
+}
